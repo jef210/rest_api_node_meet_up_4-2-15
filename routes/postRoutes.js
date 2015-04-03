c825e327a5c1e1071ab0a0fb7cc40b9f88bb7ea6
jef210 <jmalek210@gmail.com>
2015-04-02 19:49:23 -0600

    added post route

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -13,3 +13,36 @@ router.get('/posts', function(req, res, next) {
   });
 
 });
+
+// GET post
+router.get('/post/:id', function(req, res, next) {
+
+  Post.findById(req.params.id, function(err, results) {
+    if(err) {
+      res.send(err);
+    } else {
+      res.send(results);
+    }
+  });
+
+});
+
+// instance vs constructor tells which case to use 
+
+// POST post
+router.post('/posts', function(req, res, next) {
+
+  var newPost = req.body;
+  var post = new Post(newPost);
+
+// instance of post with a save method
+  
+  post.save(function(err, result){
+    if(err) {
+      res.send(err);
+    } else {
+      res.send(result);
+    }
+  });
+
+});
